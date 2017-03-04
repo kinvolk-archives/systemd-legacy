3850238097bf8be8dd9f60bad328cfceb32ab3a3
David Michael <david.michael@coreos.com>
2017-03-03 17:59:39 -0800

    Revert "resolved: actually enable mDNS support"
    This reverts commit 7bef8e8e546cbd494ea93f92394abdca0f3ed157.
    
    This mDNS change is another new service listening on the network
    that can conflict with services the user wants to run.  For now,
    revert to the previous behavior of leaving it disabled.

--- src/resolve/resolved-manager.c
+++ src/resolve/resolved-manager.c
@@ -547,7 +547,7 @@ int manager_new(Manager **ret) {
         m->hostname_fd = -1;
 
         m->llmnr_support = RESOLVE_SUPPORT_YES;
-        m->mdns_support = RESOLVE_SUPPORT_YES;
+        m->mdns_support = RESOLVE_SUPPORT_NO;
         m->dnssec_mode = DEFAULT_DNSSEC_MODE;
         m->enable_cache = true;
         m->dns_stub_listener_mode = DNS_STUB_LISTENER_UDP;
